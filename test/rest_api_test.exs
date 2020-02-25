f56f8473888b634c3f2714eeb5d7b44c03014d23
daniilperestoronin <perestoronin.daniil@gmail.com>
2020-02-25 22:34:53 +0300

    Update tests

---
+++ test/rest_api_test.exs
@@ -0,0 +1,29 @@
+defmodule RestApi.RouterTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias RestApi.Router
+
+  @content "<html><body>Hi!</body></html>"
+  @mimetype "text/html"
+
+  @opts Router.init([])
+
+  test "returns welcome" do
+    conn =
+      conn(:get, "/", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "returns 404" do
+    conn =
+      conn(:get, "/missing", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+end
--- test/rest_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RestTest do
-  use ExUnit.Case
-  doctest Rest
-
-  test "greets the world" do
-    assert Rest.hello() == :world
-  end
-end
