c9cc8880126d58ffde9aa9106393370884b15b6a
daniilperestoronin <perestoronin.daniil@gmail.com>
2020-03-06 23:07:13 +0300

    Add dummy repository

--- lib/rest_api.ex
+++ lib/rest_api.ex
@@ -2,8 +2,38 @@ defmodule RestApi.Model.User do
   defstruct [:id, :name, :email, :password, :stooge]
 end
 
+defmodule RestApi.Repository do
+  def users do
+    [
+      %RestApi.Model.User{
+        id: 1,
+        name: "Joe",
+        email: "joe@example.com",
+        password: "topsecret",
+        stooge: "moe"
+      },
+      %RestApi.Model.User{
+        id: 2,
+        name: "Bob",
+        email: "bob2@example.com",
+        password: "topsecret",
+        stooge: "moe"
+      }
+    ]
+  end
+
+  def get_all() do
+    users()
+  end
+
+  def get_by_id(id) do
+    Enum.at(users(), id)
+  end
+end
+
 defmodule RestApi.Router do
   use Plug.Router
+  require RestApi.Repository
 
   plug(:match)
   plug(:dispatch)
@@ -15,13 +45,19 @@ defmodule RestApi.Router do
       send_resp(
         conn,
         200,
-        Poison.encode!(%RestApi.Model.User{
-          id: 1,
-          name: "Joe",
-          email: "joe@example.com",
-          password: "topsecret",
-          stooge: "moe"
-        })
+        RestApi.Repository.get_all()
+        |> Poison.encode!()
+      )
+  )
+
+  get("/:id",
+    do:
+      send_resp(
+        conn,
+        200,
+        elem(Integer.parse(id), 0)
+        |> RestApi.Repository.get_by_id()
+        |> Poison.encode!()
       )
   )
 
